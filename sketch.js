f4bfa22300ba9d99aefad6bc9e4e600175986e64
hbz2211 <44384827+hbz2211@users.noreply.github.com>
2018-10-24 21:25:20 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -23,22 +23,44 @@ function draw() {
 }
 
 function ball(){
+    this.index;
 	this.x = 0;
 	this.y = 0;
 	this.speed = createVector(0,0)
 	this.a = 0;
-	this.r = 25
-	this.ballStop = canvas.height - this.r
+    this.r = 0
+    this.nextY;
 	this.display = function(){
 		ellipse(this.x, this.y, this.r*2, this.r*2);
 	}
 	this.grav = function(){
-		if(this.y + this.speed.y < this.ballStop){
-			this.y += this.speed.y
-			this.speed.y += a;
-		}else if (this.y + this.speed.y >= this.ballStop){
-			this.speed.y = 0
-			this.y = this.ballStop
-		} 
-	}
+        this.ballStop = int(canvas.height - this.r)
+        if(this.y + this.speed.y < this.ballStop){
+            this.y += this.speed.y;
+            this.speed.y += this.a;
+            this.a += this.a
+        }else if(this.y + this.speed.y >= this.ballStop){
+            this.speed.y = 0
+            this.y = this.ballStop
+        }
+        if(this.x < this.r){
+            this.x = this.r
+        }else if(this.x > canvas.width - this.r){
+            this.x = canvas.width - this.r
+        }
+    }
+    // this.colide = function(){
+    //     var n = index+1
+    //     this.distance = dist(this.y, this.x, objs[n].y,objs[n].x)
+    //     if (this.distance < this.r + objs[n].r){
+    //         this.speed.x = 0
+    //         this.speed.y = 0
+    //         objs[n].speed.x = 0
+    //         objs[n].speed.y = 0
+            
+        
+    //     console.log(this.distance)
+    //     }
+
+    // }
 }
